2ca7b285082d9f76ab238bb4a0db8698f052ef40
Alexander <al.deimosfrontend@gmail.com>
2020-05-13 21:19:20 +0300

    Updates

---
+++ precache-manifest.d69bff0866b162ff0ab1ebadb128d883.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f70779de8dd54a8c58a26f2af34afece",
+    "url": "/vk-freshcode/index.html"
+  },
+  {
+    "revision": "a7a299e531877292d4c0",
+    "url": "/vk-freshcode/static/css/2.551d3f2e.chunk.css"
+  },
+  {
+    "revision": "30bd8e55eb4d99ec1ccd",
+    "url": "/vk-freshcode/static/css/main.7883ad8d.chunk.css"
+  },
+  {
+    "revision": "a7a299e531877292d4c0",
+    "url": "/vk-freshcode/static/js/2.ac4bf149.chunk.js"
+  },
+  {
+    "revision": "0b08544f54c9906705be39c03b7e79ad",
+    "url": "/vk-freshcode/static/js/2.ac4bf149.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30bd8e55eb4d99ec1ccd",
+    "url": "/vk-freshcode/static/js/main.240a6547.chunk.js"
+  },
+  {
+    "revision": "f6baaa0e4731198ee7be",
+    "url": "/vk-freshcode/static/js/runtime-main.67a10482.js"
+  },
+  {
+    "revision": "bfddc2a3b6bc31c9c25a8f391450dd00",
+    "url": "/vk-freshcode/static/media/SFProDisplay-Semibold.bfddc2a3.ttf"
+  },
+  {
+    "revision": "a260cbc18870da144038776461d9df28",
+    "url": "/vk-freshcode/static/media/SFProText-Medium.a260cbc1.ttf"
+  },
+  {
+    "revision": "85bd46c1cff02c1d8360cc714b8298fa",
+    "url": "/vk-freshcode/static/media/SFProText-Regular.85bd46c1.ttf"
+  },
+  {
+    "revision": "35313ea7fc3aa7172a3627b3666d6711",
+    "url": "/vk-freshcode/static/media/TTCommons-DemiBold.35313ea7.ttf"
+  }
+]);
